731e820376eda98d8afbee3853ba4e3d1bf05712
Andrew Blowe <andrewblowe@Andrews-MacBook-Pro.local>
2015-04-17 16:13:17 -0400

    added sankey and first treemap

--- app/assets/javascripts/organization_type_sankey.js
+++ app/assets/javascripts/organization_type_sankey.js
@@ -6,7 +6,46 @@ var qd = require('quickdash-core'),
     fastclick = require('fastclick'),
     dtip = require('d3-tip')(d3);
 
-console.log("Hello world!!!");
-
 // var df = require('./lib/bubblemap-factories'),
 //     dataConfig = require('./lib/bubblemap-data-config');
+
+$(document).ready(function(){
+	loadDataAndRender($('#year-select').val())
+
+	//load data based on year selected
+	$('#year-select').on("change", function(){
+		loadDataAndRender(this.value);
+	});
+
+//make request for data based on year and load data for the chart
+function loadDataAndRender(year) {
+	$('#loading').removeClass('loaded');
+
+	//make the request
+	$.get("proposals/submission_fiscal_year/" + year + ".json", function(data, status) {
+		crossfilterData = crossfilter(data);
+		orgTypeDimension = crossfilterData.dimension(function(d) {return d.ORGANIZATION_TYPE;});
+		programAreaDimension = crossfilterData.dimension(function(d) {return d.PROGRAM_AREA_NAME;})
+		awardsOrgType = crossfilterData.dimension(function(d) {return d.award_organization_type;})
+
+		var levels = [{'dimension': orgTypeDimension, 'columnName': 'ORGANIZATION_TYPE'},
+					  {'dimension': programAreaDimension, 'columnName': 'PROGRAM_AREA_NAME'},
+					  {'dimension': awardsOrgType, 'columnName': 'award_organization_type'}];
+
+		proposalsSankey = dc.sankey("#organization-type-sankey")
+								.width(900)
+								.height(500)
+								.levels(levels)
+								.measureColumn('measure')
+								.nodeWidth(30);
+		dc.renderAll();
+		$('#loading').addClass('loaded');
+		console.log(data[0]);
+	});
+}
+
+});
+
+
+
+
--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -4,7 +4,7 @@ class ProposalsController < ApplicationController
 		year = params[:year]
 
 		#MAKE SURE to change AWARD_FISCAL_YEAR to SUBMISSION_FISCAL_YEAR !!IMPORTANT
-		db_query = Proposal.where("AWARD_FISCAL_YEAR = '#{year}'")
+		db_query = Proposal.where("Fiscal_Year = '#{year}'")
 
 		respond_to do |format|
 			format.json {render json: db_query}
